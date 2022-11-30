ac90bbaa1ef27bb87e30f9981be5893a8c1968e2
Adam Coogan <dr.adam.coogan@gmail.com>
2022-11-30 12:33:17 -0500

    try to fix docs by including md suffix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,6 +33,7 @@ extensions = [
 
 templates_path = ["_templates"]
 exclude_patterns = []
+source_suffix = [".md"]
 
 
 # -- Options for HTML output -------------------------------------------------
